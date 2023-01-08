32ddcc324b8ccb09950fec86afa1779fed488ae4
tom-deman <49947427+tom-deman@users.noreply.github.com>
2023-01-08 15:29:19 +0100

    search form visual done

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,5 +1,28 @@
+import search from '../assets/img/690150.png'
+
 const SearchField = () => {
-    return 'SearchField'
+
+    const searchVat = () => {
+        alert( 'search now' )
+    }
+
+    return(
+        <div className="flex mt-8">
+            <input
+                type="text"
+                id="tva-search"
+                className="bg-blue-100 w-11/12 py-2 px-6 outline-none shadow-lg border border-blue-200 shadow-blue-50"
+            />
+            <button className="w-1/12 border-blue-200 border border-l-0 hover:bg-blue-50 flex justify-center items-center">
+                <img
+                    src={ search }
+                    alt="magnifier icon"
+                    width="26"
+                    onClick={ () => searchVat() }
+                />
+            </button>
+        </div>
+    )
 }
 
 export default SearchField
