2902d254440f7a5510e8931d34be1fefc46e071a
tom-deman <49947427+tom-deman@users.noreply.github.com>
2023-01-08 15:36:21 +0100

    footer created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react"
+
 const Footer = () => {
-    return 'Footer'
+    const currentYear = useState( new Date().getFullYear() )
+
+    return(
+        <footer className="fixed bottom-0 py-10 bg-neutral-50 border border-gray-100 border-t-1 w-full">
+            <p className="text-center text-sm font-light">
+                © { currentYear } - Thomas Deman - All Rights Reserved
+            </p>
+        </footer>
+    )
 }
 
 export default Footer
