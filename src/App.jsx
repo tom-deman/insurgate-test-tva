d5eb7676abc3d582f7da19139c294ae12747b497
tom-deman <49947427+tom-deman@users.noreply.github.com>
2023-01-08 14:44:33 +0100

    basic navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import './assets/css/tailwind.css'
 import './assets/css/main.css'
 
 import Nav from './components/Nav'
+import AppDescription from './components/AppDescription'
 import SearchField from './components/SearchField'
 import Result from './components/Result'
 import Footer from './components/Footer'
 
 const App = () => {
     return(
-        <div>
-            <Nav></Nav>
-            <SearchField></SearchField>
-            <Result></Result>
-            <Footer></Footer>
+        <div className='h-screen w-screen'>
+            <header>
+                <Nav />
+            </header>
+            <main>
+                <AppDescription />
+                <SearchField />
+                <Result />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,53 @@
+import logo from '../assets/img/logo.png'
+
 const Nav = () => {
-    return 'Nav'
+    return(
+        <nav className='flex justify-around py-6 shadow-md shadow-gray-100'>
+            <a
+                href="https://www.insurgate.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                    src={ logo }
+                    alt="logo.png"
+                    width="138"
+                />
+            </a>
+            <ul className='flex'>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/thomas-deman-168562193/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='hover:text-blue-700'
+                    >
+                        Linkedin
+                    </a>
+                </li>
+                <li className='px-20'>
+                    <a
+                        href="https://github.com/tom-deman"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='hover:text-blue-700'
+                    >
+                        Github
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://tom-deman.github.io/portfolio/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='hover:text-blue-700'
+                    >
+                        Portfolio
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav
