7c4d27d04f66de6cc28350824b62c5fb6cfae9ae
tom-deman <49947427+tom-deman@users.noreply.github.com>
2023-01-08 13:55:03 +0100

    compiling tailwindcss to check if everything is ok + Axios installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import './assets/css/tailwind.css'
+
+import Dummy from './components/Dummy'
+
 const App = () => {
-    return 'Test'
+    return(
+        <div>
+            <Dummy />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Dummy.jsx
@@ -0,0 +1,11 @@
+const Dummy = () => {
+    return(
+        <>
+            <h1 className="text-blue-400">
+                Hello World !
+            </h1>
+        </>
+    )
+}
+
+export default Dummy
